724a58d392763c65e7a008128dfc0ce4a08a52af
Minhaz Panara <minhaz.panara@gmail.com>
2020-10-06 22:56:30 +0530

    Initial Commit

---
+++ ios-food-delivery-viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-food-delivery-viper
+//
+//  Created by Minhaz Panara on 10/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
